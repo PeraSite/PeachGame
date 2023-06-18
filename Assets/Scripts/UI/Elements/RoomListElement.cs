198c7f05d53078239cb6a04c103e48548cf2cfe3
PeraSite <pretocki3@gmail.com>
2023-06-19 01:05:39 +0900

    feat: 방 접속 버튼 기능 추가

--- Assets/Scripts/UI/Elements/RoomListElement.cs
+++ Assets/Scripts/UI/Elements/RoomListElement.cs
@@ -1,12 +1,15 @@
 ﻿using PeachGame.Common.Models;
+using PeachGame.Common.Packets.Client;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace PeachGame.Client.UI.Elements {
 	public class RoomListElement : MonoBehaviour {
 		[SerializeField] private TextMeshProUGUI _roomNameText;
 		[SerializeField] private TextMeshProUGUI _playersText;
 		[SerializeField] private TextMeshProUGUI _stateText;
+		[SerializeField] private Button _joinButton;
 
 		public void Setup(RoomInfo info) {
 			_roomNameText.text = info.Name;
@@ -19,6 +22,20 @@ namespace PeachGame.Client.UI.Elements {
 				_ => "알 수 없음"
 			};
 			_stateText.text = $"상태 : {state}";
+
+			_joinButton.onClick.AddListener(() => {
+				if (info.CurrentPlayers >= info.MaxPlayers) {
+					Debug.LogError("방이 꽉 찼습니다.");
+					return;
+				}
+
+				if (info.State != RoomState.Waiting) {
+					Debug.LogError("방이 대기 상태가 아닙니다.");
+					return;
+				}
+
+				NetworkManager.Instance.SendPacket(new ClientRequestJoinRoomPacket(info.RoomId));
+			});
 		}
 	}
 }
