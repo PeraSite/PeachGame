2e9a3c0f9a3e1edbddab05a9390c84e59c8a2dad
PeraSite <pretocki3@gmail.com>
2023-06-19 00:58:01 +0900

    feat: 방 목록 표기 추가

---
+++ Assets/Scripts/UI/Elements/RoomListElement.cs
@@ -0,0 +1,24 @@
+﻿using PeachGame.Common.Models;
+using TMPro;
+using UnityEngine;
+
+namespace PeachGame.Client.UI.Elements {
+	public class RoomListElement : MonoBehaviour {
+		[SerializeField] private TextMeshProUGUI _roomNameText;
+		[SerializeField] private TextMeshProUGUI _playersText;
+		[SerializeField] private TextMeshProUGUI _stateText;
+
+		public void Setup(RoomInfo info) {
+			_roomNameText.text = info.Name;
+			_playersText.text = $"플레이어 : {info.CurrentPlayers}/{info.MaxPlayers}";
+
+			var state = info.State switch {
+				RoomState.Waiting => "대기",
+				RoomState.Playing => "플레이",
+				RoomState.Ending => "종료",
+				_ => "알 수 없음"
+			};
+			_stateText.text = $"상태 : {state}";
+		}
+	}
+}
--- Assets/Scripts/UI/RoomListUI.cs
+++ Assets/Scripts/UI/RoomListUI.cs
@@ -1,12 +1,65 @@
-﻿using TMPro;
+﻿using System.Collections.Generic;
+using PeachGame.Client.UI.Elements;
+using PeachGame.Common.Packets.Client;
+using PeachGame.Common.Packets.Server;
+using TMPro;
 using UnityEngine;
 
 namespace PeachGame.Client.UI {
-	public class RoomListUI : MonoBehaviour {
+	public class RoomListUI : MonoBehaviour, IPacketHandler<ServerResponseRoomListPacket> {
+		[Header("방 목록")]
+		[SerializeField] private RoomListElement _roomListElementPrefab;
+		[SerializeField] private RectTransform _roomListParent;
+
+
+		[Header("닉네임 설정")]
 		[SerializeField] private TMP_InputField _nicknameInput;
 
+		private void Awake() {
+			_roomListElements = new List<RoomListElement>();
+		}
+
 		private void Start() {
+			SetupNickname();
+			RefreshRoomList();
+		}
+
+		private void OnEnable() {
+			NetworkManager.Instance.RegisterPacketHandler(this);
+		}
+
+		private void OnDisable() {
+			NetworkManager.Instance.UnregisterPacketHandler(this);
+		}
+
+#region 닉네임 설정
+		private void SetupNickname() {
+			// Join 씬에서 정한 닉네임 설정
 			_nicknameInput.text = NetworkManager.Instance.Nickname;
 		}
+
+		public void UpdateNickname() {
+			// 새로운 닉네임으로 값 업데이트
+			NetworkManager.Instance.Nickname = _nicknameInput.text;
+		}
+#endregion
+
+#region 방 목록
+		private List<RoomListElement> _roomListElements;
+
+		public void RefreshRoomList() {
+			NetworkManager.Instance.SendPacket(new ClientRequestRoomListPacket());
+		}
+
+		public void Handle(ServerResponseRoomListPacket packet) {
+			_roomListElements.ForEach(x => Destroy(x.gameObject));
+			_roomListElements.Clear();
+			packet.InfoList.ForEach(roomInfo => {
+				var roomListElement = Instantiate(_roomListElementPrefab, _roomListParent);
+				roomListElement.Setup(roomInfo);
+				_roomListElements.Add(roomListElement);
+			});
+		}
+#endregion
 	}
 }
